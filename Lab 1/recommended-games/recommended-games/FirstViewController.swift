59046254ac71024406efec30cb8bc24d54750c85
Mitchell Wolfe <speedywolfe@gmail.com>
2016-01-25 22:11:15 -0700

    add ze files

---
+++ Lab 1/recommended-games/recommended-games/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  recommended-games
+//
+//  Created by Mitchell Wolfe on 1/25/16.
+//  Copyright © 2016 Mitchell Wolfe. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Lab 1/recommended-games/recommended-games/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  recommended-games
+//
+//  Created by Mitchell Wolfe on 1/25/16.
+//  Copyright © 2016 Mitchell Wolfe. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
