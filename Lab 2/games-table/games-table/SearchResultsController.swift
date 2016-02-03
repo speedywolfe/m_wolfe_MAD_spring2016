f17a88cca19798b80636e70c1627915966fef8a4
Mitchell Wolfe <speedywolfe@gmail.com>
2016-02-02 21:43:52 -0700

    Sectioned tables working

---
+++ Lab 2/games-table/games-table/SearchResultsController.swift
@@ -0,0 +1,13 @@
+//
+//  SearchResultsController.swift
+//  games-table
+//
+//  Created by Mitchell Wolfe on 2/2/16.
+//  Copyright © 2016 Mitchell Wolfe. All rights reserved.
+//
+
+import Cocoa
+
+class SearchResultsController: UITableViewController {
+
+}
--- Lab 2/games-table/games-table/ViewController.swift
+++ Lab 2/games-table/games-table/ViewController.swift
@@ -8,11 +8,50 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
+    
+    var games : [String : [String]]!
+    var headers : [String]!
+    var searchController : UISearchController!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let path = NSBundle.mainBundle().pathForResource("games", ofType: "plist")
+        games = NSDictionary(contentsOfFile: path!) as! [String : [String]]
+        headers = Array(games.keys)
+    }
+    
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        let header = headers[section]
+        let headerSection = games[header]! as [String]
+        return headerSection.count
+    }
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let section = indexPath.section
+        let header = headers[section]
+        let headerSection = games[header]! as [String]
+        let cell = tableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath)
+        cell.textLabel?.text = headerSection[indexPath.row]
+        cell.detailTextLabel?.text = "Replace this with something"
+        return cell
+    }
+    
+    override func tableView(tableView: UITableView, didDeselectRowAtIndexPath indexPath: NSIndexPath) {
+        tableView.deselectRowAtIndexPath(indexPath, animated: true)
+    }
+    
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return headers.count
+    }
+    
+    override func tableView(tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return headers[section]
+    }
+    
+    override func sectionIndexTitlesForTableView(tableView: UITableView) -> [String]? {
+        return headers
     }
 
     override func didReceiveMemoryWarning() {
