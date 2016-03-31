5a1c7d660f6e2659ea44c3c52a3e8368719b11ba
Mitchell Wolfe <speedywolfe@gmail.com>
2016-03-30 21:40:49 -0600

    start lab 8

---
+++ Lab 8/games-firebase/games-firebase/AddViewController.swift
@@ -0,0 +1,38 @@
+//
+//  AddViewController.swift
+//  games-firebase
+//
+//  Created by Mitchell Wolfe on 3/30/16.
+//  Copyright © 2016 Mitchell Wolfe. All rights reserved.
+//
+
+import UIKit
+
+class AddViewController: UIViewController {
+
+    @IBOutlet weak var gamename: UITextField!
+    @IBOutlet weak var gameurl: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
